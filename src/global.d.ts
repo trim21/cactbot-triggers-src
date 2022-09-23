da2e630599aa75175bb2f61733d7118b15a17a19
Trim21 <trim21.me@gmail.com>
2022-09-23 21:15:39 +0800

    update

--- src/global.d.ts
+++ src/global.d.ts
@@ -8,22 +8,16 @@ import _Util from '../../cactbot/resources/util';
 import _ZoneId from '../../cactbot/resources/zone_id';
 import _ZoneInfo from '../../cactbot/resources/zone_info';
 
-import _JobsOptions from '../../cactbot/ui/jobs/jobs_options';
-import _OopsyRaidsyOptions from '../../cactbot/ui/oopsyraidsy/oopsy_options';
 import _RaidbossOptions from '../../cactbot/ui/raidboss/raidboss_options';
-import { BaseOptions } from '../../cactbot/types/data';
-import { HuntMap } from '../../cactbot/resources/hunt';
 import type { RaidbossData } from 'cactbot/data';
 import type { TriggerSet } from 'cactbot/trigger';
-
-type LocaleTextOrArray = LocaleObject<string | string[]>;
 
 declare global {
   const Options: {
     Triggers: {
       push<T extends RaidbossData>(item: TriggerSet<T>)
     };
-  }
+  };
   // Global variables
   const Conditions: typeof _Conditions;
   const ContentType: typeof _ContentType;
@@ -35,37 +29,5 @@ declare global {
   const ZoneId: typeof _ZoneId;
   const ZoneInfo: typeof _ZoneInfo;
   // options of modules
-  const JobsOptions: typeof _JobsOptions;
   const RaidbossOptions: typeof _RaidbossOptions;
-  const OopsyRaidsyOptions: typeof _OopsyRaidsyOptions;
-  // TODO: use cactbot built-in types when available
-  const RadarOptions: BaseOptions & {
-    BRankEnabled: boolean;
-    DetectionRange: number;
-    TTS: boolean;
-    PopSoundAlert: boolean;
-    PopVolume: number;
-    Puller: boolean;
-    Position: boolean;
-    PopSound: string;
-    RankOptions: {
-      [rank: string]: {
-        Type?: 'mob' | 'any';
-        Enabled?: boolean;
-        DetectionRange?: number;
-        TTS?: boolean;
-        PopVolume?: number;
-        PopSoundAlert?: boolean;
-      };
-    };
-    CustomMonsters: {
-      [huntName: string]: {
-        id?: string;
-        name?: LocaleTextOrArray | string | string[];
-        rank?: Rank;
-        regex?: RegExp;
-        hp?: number;
-      };
-    };
-  };
 }
