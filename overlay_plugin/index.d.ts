5f2d00a5f03bad451f4c6ee39ad9bc78373366fa
Trim21 <trim21.me@gmail.com>
2022-10-07 17:25:24 +0800

    update type definition

--- overlay_plugin/index.d.ts
+++ overlay_plugin/index.d.ts
@@ -1,8 +1,15 @@
-import { EventMap, EventType } from 'cactbot/types/event';
+export interface Party {
+  id: string;
+  name: string;
+  worldId: number;
+  job: number;
+  inParty: boolean;
+}
 
 declare global {
   namespace OverlayPlugin {
-    function addOverlayListener<T extends EventType>(event: T, cb: EventMap[T]): void;
+    function addOverlayListener(event: 'LogLine', cb: (ev: { type: 'LogLine'; line: string[]; rawLine: string; }) => void): void;
+    function addOverlayListener(event: 'PartyChanged', cb: (ev: { type: 'PartyChanged'; party: Party[]; }) => void): void;
 
     function callOverlayHandler(msg: { call: 'saveData'; key: string; data: unknown }): Promise<void>;
     function callOverlayHandler<T = unknown>(msg: { call: 'loadData'; key: string; }): Promise<({ data?: T } | undefined)>;
